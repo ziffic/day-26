9d96e905b37b8bcdd2a8b97e3c6b4b4aa5888fdb
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-15 16:53:09 -0700

    DataFrame iteration

--- main.py
+++ main.py
@@ -20,14 +20,27 @@
 # print(result)
 
 # CHALLENGE FIVE
-weather_c = {
-    "Monday": 12,
-    "Tuesday": 14,
-    "Wednesday": 15,
-    "Thursday": 14,
-    "Friday": 21,
-    "Saturday": 22,
-    "Sunday": 24,
+# weather_c = {
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+#     "Sunday": 24,
+# }
+# weather_f = {day:temp * 9/5 + 32 for (day, temp) in weather_c.items()}
+# print(weather_f)
+
+import pandas
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
 }
-weather_f = {day:temp * 9/5 + 32 for (day, temp) in weather_c.items()}
-print(weather_f)
+
+student_data_frame = pandas.DataFrame(student_dict)
+# print(student_data_frame)
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    print(row)
