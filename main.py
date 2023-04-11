baf5d0f340256c27fe74e3b43bce0ced50ede27f
David Yrigoyen <dave@dryweb.com>
2023-04-10 20:39:24 -0700

    refactor

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ student_data_frame = pandas.DataFrame(student_dict)
 # print(student_data_frame)
 # Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    print(row)
+    print(row.student)
